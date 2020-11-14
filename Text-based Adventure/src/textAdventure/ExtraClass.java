afef82bc867fd8f89beca5810aa3b4e3fa69d5d2
mblai <mblai311@gmail.com>
2020-11-14 00:08:29 -0700

    My First Commit With Eclipse

---
+++ Text-based Adventure/src/textAdventure/ExtraClass.java
@@ -0,0 +1,35 @@
+package textAdventure;
+
+import java.util.Random;
+import java.util.Scanner;
+import java.util.Random;
+
+public class ExtraClass {
+	
+	//ENEMY CHARACTERISTICS
+	final String[] boss= {"King Trump"};
+	final String[] enemies1= {"Orc", "Troll", "Goblin", "Assassin", "Enraged Goat" };
+	final String[] enemies2= {"Dirty Magician", "Diddlin' Priest", "Sexy Preteen", "Legendary Weeaboo"};
+	int maxEnemyHealth= 175;
+	int maxAttackDamage= 60;
+	int maxEnemyDefence= 15;
+
+
+	//PLAYER CHARACTERISTICS
+	int health= 300;
+	int attackDamage= 65;
+	int rapeAttack= 100;
+	int numHealthPotions= 3;
+	int healthPotionHeal= 150;
+	int potionDropChance= 50; //PERCENT CHANCE
+
+
+}
+
+
+	
+
+
+
+
+	
---
+++ Text-based Adventure/src/textAdventure/TextAdventureMain.java
@@ -0,0 +1,25 @@
+package textAdventure;
+import java.util.Scanner;
+import java.util.InputMismatchException;
+import java.util.Random;
+public class TextAdventureMain {
+
+	public TextAdventureMain() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+}
+
+	
+	
+	
+	
+	
+	
+	
+	
